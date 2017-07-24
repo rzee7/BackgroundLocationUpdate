df22951dd65d2adfd205d34d3f4797c995aabdba
RIYAZ <rzee.m7@gmail.com>
2017-07-24 21:52:33 +0530

    Add project files.

---
+++ LocationUpdate/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace LocationUpdate
+{
+    [Activity(Label = "LocationUpdate", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
